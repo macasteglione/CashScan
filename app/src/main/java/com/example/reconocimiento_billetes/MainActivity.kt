2eea403d94698a02123dc90bf7b8ae9097d32485
matiascasteglione2002 <matiascasteglione2002@gmail.com>
2024-09-23 15:50:02 -0300

    agregue menu

--- app/src/main/java/com/example/reconocimiento_billetes/MainActivity.kt
+++ app/src/main/java/com/example/reconocimiento_billetes/MainActivity.kt
@@ -11,10 +11,10 @@ import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
@@ -23,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,9 +47,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ReconocimientobilletesTheme {
-                var classifications by remember {
-                    mutableStateOf(emptyList<Classification>())
-                }
+                var showCamera by remember { mutableStateOf(false) }
+                var classifications by remember { mutableStateOf(emptyList<Classification>()) }
+
+                // Image Analyzer setup
                 val analyzer = remember {
                     BilletesImageAnalyzer(
                         classifier = TfLiteBilletesClassifier(
@@ -59,6 +61,7 @@ class MainActivity : ComponentActivity() {
                         }
                     )
                 }
+
                 val controller = remember {
                     LifecycleCameraController(applicationContext).apply {
                         setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
@@ -68,33 +71,44 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                 }
+
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
                 ) {
-                    CameraPreview(controller, Modifier.fillMaxSize())
-
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .align(Alignment.TopCenter)
-                    ) {
-                        classifications.forEach {
-                            val labels = loadLabels(applicationContext)
-                            val label = if (it.index < labels.size) labels[it.index] else "Desconocido"
-
-                            Text(
-                                text = label,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(MaterialTheme.colorScheme.primaryContainer)
-                                    .padding(8.dp),
-                                textAlign = TextAlign.Center,
-                                fontSize = 20.sp,
-                                color = MaterialTheme.colorScheme.primary
-                            )
+                    if (showCamera) {
+                        CameraPreview(controller, Modifier.fillMaxSize())
+                    } else {
+                        Button(
+                            onClick = { showCamera = true },
+                            modifier = Modifier.align(Alignment.Center)
+                        ) {
+                            Text(text = "Abrir Cámara")
                         }
                     }
+
+                    // Mostrar el billete reconocido en la parte superior
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.primaryContainer)
+                                .align(Alignment.TopCenter)
+                        ) {
+                            classifications.forEach {
+                                val labels = loadLabels(applicationContext)
+                                val label = if (it.index < labels.size) labels[it.index] else "Desconocido"
+
+                                Text(
+                                    text = label,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(0.dp, 40.dp, 0.dp, 16.dp),
+                                    fontSize = 20.sp,
+                                    color = Color.White,
+                                    textAlign = TextAlign.Center,
+                                )
+                            }
+                        }
                 }
             }
         }
@@ -107,11 +121,9 @@ class MainActivity : ComponentActivity() {
     private fun loadLabels(context: Context): List<String> {
         val labels = mutableListOf<String>()
         val inputStream = context.assets.open("labels.txt")
-
         inputStream.bufferedReader().useLines { lines ->
             lines.forEach { labels.add(it) }
         }
-
         return labels
     }
 }
