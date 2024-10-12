6aa27664b0c5bb5ecedcebb42b1fbcf55370b41a
matiascasteglione2002 <matiascasteglione2002@gmail.com>
2024-10-12 16:57:52 -0300

    agregue linterna temporizado

--- movil/app/src/main/java/com/example/reconocimiento_billetes/presentation/LuminosityAnalyzer.kt
+++ movil/app/src/main/java/com/example/reconocimiento_billetes/presentation/LuminosityAnalyzer.kt
@@ -2,14 +2,31 @@ package com.example.reconocimiento_billetes.presentation
 
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import android.os.SystemClock
 
-class LuminosityAnalyzer(val onLowLightDetected: (Boolean) -> Unit) : ImageAnalysis.Analyzer {
+class LuminosityAnalyzer(
+    val onLowLightDetected: (Boolean) -> Unit
+) : ImageAnalysis.Analyzer {
+
+    private var lastTorchToggleTime: Long = 0
+    private val torchToggleThreshold: Long = 8000
+
     override fun analyze(image: ImageProxy) {
         val buffer = image.planes[0].buffer
         val pixels = ByteArray(buffer.capacity())
         buffer.get(pixels)
 
         val luminance = pixels.map { it.toInt() and 0xFF }.average()
-        onLowLightDetected(luminance < 50)
+
+        // Obtener el tiempo actual
+        val currentTime = SystemClock.elapsedRealtime()
+
+        // Verificar si han pasado al menos 2 segundos desde el último cambio de linterna
+        if (currentTime - lastTorchToggleTime >= torchToggleThreshold) {
+            onLowLightDetected(luminance < 50)
+            lastTorchToggleTime = currentTime
+        }
+
+        image.close()
     }
 }
